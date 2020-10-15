7b6f9648a45d0f90b28e80dfff824f239730dd2c
Héber Silvério <hebersilverio@gmail.com>
2020-10-14 22:09:04 -0300

    Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { FiArrowRight } from 'react-icons/fi';
 import './styles/global.css';
 import './styles/pages/landing.css'
 import logoImg from './images/Logo.svg'
@@ -8,21 +8,22 @@ function App() {
   return (
     <div id="page-landing">
           <div className="content-wrapper">
-            <img src={logoImg} alt="Happy"/>
+              <img src={logoImg} alt="Happy"/>
           
           
-            <main>
-              <h1>Leve Felicidade para o mundo</h1>
-              <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-            </main>
+              <main>
+                <h1>Leve Felicidade para o mundo</h1>
+                <p>Visite orfanatos e mude o dia <br/>de muitas crianças.</p>
+              </main>
 
-            <div className="location">
-              <strong>Goiânia-GO</strong> 
-            </div>
-            
-            <a href=" " className="enter-app">
-              >
-            </a>
+              <div className="location">
+                <strong>Goiânia </strong> 
+                <span> Goiás</span>
+              </div>
+              
+              <a href=" " className="enter-app">
+                <FiArrowRight size={26} color="rgb(0,0,0,)" />
+              </a>
           </div>
     </div>
   );
